fe114ebb7e188497d5c51be45cb9ce5f9b31d8ab
Sailaja Mohanty <sailajamohanty@Sailajas-MBP.fritz.box>
2024-08-05 23:30:13 +0100

    new project in python

---
+++ hello.py
@@ -0,0 +1,4 @@
+msg = "hello"
+print(msg)
+msg = "sailaja"
+print(msg)
---
+++ program.py
@@ -0,0 +1,12 @@
+
+from datetime import date
+#date.today()
+#print(date.today())
+print("Today's date is: " + str(date.today()))
+
+# program.py
+sum = 1 + 2
+print(sum)
+parsecs = 11
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
