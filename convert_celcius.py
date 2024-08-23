58f0f5e7686b9920d9f553bd68a40ee64646e189
Sailaja Mohanty <sailajamohanty@Sailajas-MBP.fritz.box>
2024-08-23 13:40:10 +0100

    some new files added

--- convert_celcius.py
+++ convert_celcius.py
@@ -1,6 +1,24 @@
 # Function to convert Fahrenheit to Celsius
 
 
+
+
+
+# This program converts temperature from Fahrenheit to Celsius
+
+# Ask the user to enter a temperature in Fahrenheit
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+# Convert the temperature to Celsius using the formula
+celsius = (fahrenheit - 32) * 0.556
+
+# Print the result
+print(f"The temperature in Celsius is: {celsius:.2f}")
+
+
+
+
+
 print("THIS PROGRAM WILL CONVERT DEGREES FARENHEIT")
 
 def fahrenheit_to_celsius(F):
--- stringmethod.py
+++ stringmethod.py
@@ -1,3 +1,60 @@
+#https://learn.microsoft.com/en-us/training/modules/python-strings/3-string-methods
+
+
 heading = "temperatures and facts about the moon"
 heading_upper = heading.title()
 print(heading_upper)
+
+
+temperatures = "Daylight: 260 F Nighttime: -280 F"
+temperatures_list = temperatures.split()
+print(temperatures_list)
+
+
+print("Moon" in "This text will describe facts and challenges with space travel")
+print("Moon" in "This text will describe facts about the Moon")
+
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.find("Moon"))
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.find("Mars"))
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.count("Mars"))
+print(temperatures.count("Moon"))
+
+print("The Moon And The Earth".lower())
+print("The Moon And The Earth".upper())
+
+temperatures = "Mars Average Temperature: -60 C"
+
+temperatures = "Mars Average Temperature: -60 C"
+parts = temperatures.split(':')
+print(parts)
+print(parts[-1])
+
+#looping over
+mars_temperature = "The highest temperature on Mars is about 30 C"
+for item in mars_temperature.split():
+    if item.isnumeric():
+        print(item)
+
+print("-60".startswith('-'))
+
+
+if "30 C".endswith("C"):
+ print("This temperature is in Celsius")
+
+
+print("Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.".replace("Celsius", "C"))
+
+text = "Temperatures on the Moon can vary wildly."
+print("temperatures" in text)
+
+text = "Temperatures on the Moon can vary wildly."
+print("temperatures" in text.lower())
+
+moon_facts = ["The Moon is drifting away from the Earth.", "On average, the Moon is moving about 4cm every year."]
+print(' '.join(moon_facts))
